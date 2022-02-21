da19918576a65a63406aaaff2acc6ca18e6d1dbe
Akash21Thakur <akashthakur562002@gmail.com>
2022-02-21 23:47:28 +0530

    21-02-2022

--- PRACTICE/dsa_450/Array/35medianEqualSize.cpp
+++ PRACTICE/dsa_450/Array/35medianEqualSize.cpp
@@ -1,27 +1,16 @@
-// A Simple Merge based O(n) solution to find median of
-// two sorted arrays
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
 
-// From GFG
-
-/* This function returns median of ar1[] and ar2[].
-Assumptions in this function:
-Both ar1[] and ar2[] are sorted arrays
-Both have n elements */
-int getMedian(int ar1[], int ar2[], int n)
+int findMed(int *ar1, int *ar2, int n)
 {
-    int i = 0; /* Current index of i/p array ar1[] */
-    int j = 0; /* Current index of i/p array ar2[] */
-    int count;
-    int m1 = -1, m2 = -1;
+    int i = 0; // ar1 index
+    int j = 0; // ar2 index
 
-    /* Since there are 2n elements, median will be average
-    of elements at index n-1 and n in the array obtained after
-    merging ar1 and ar2 */
-    for (count = 0; count <= n; count++)
+    int m1 = -1, m2 = -1; // median cur and prev
+    for (int count = 0; count <= n; count++)
     {
-        /*Below is to handle case where all elements of ar1[] are
-        smaller than smallest(or first) element of ar2[]*/
+        // if ar1 largest < ar2 smallest
         if (i == n)
         {
             m1 = m2;
@@ -29,46 +18,27 @@ int getMedian(int ar1[], int ar2[], int n)
             break;
         }
 
-        /*Below is to handle case where all elements of ar2[] are
-        smaller than smallest(or first) element of ar1[]*/
-        else if (j == n)
+        // if ar2 largest < ar1 smallest
+        if (j == n)
         {
             m1 = m2;
             m2 = ar1[0];
             break;
         }
-        /* equals sign because if two
-            arrays have some common elements */
+
         if (ar1[i] <= ar2[j])
         {
-            m1 = m2; /* Store the prev median */
-            m2 = ar1[i];
+            m1 = m2;     // store the prev median
+            m2 = ar1[i]; // current median
             i++;
         }
         else
         {
-            m1 = m2; /* Store the prev median */
-            m2 = ar2[j];
+            m1 = m2;     // store the prev median
+            m2 = ar2[j]; // current median
             j++;
         }
     }
 
     return (m1 + m2) / 2;
-}
-
-/* Driver program to test above function */
-// AKASH THAKUR
-int main()
-{
-    int ar1[] = {1, 12, 15, 26, 38};
-    int ar2[] = {2, 13, 17, 30, 45};
-
-    int n1 = sizeof(ar1) / sizeof(ar1[0]);
-    int n2 = sizeof(ar2) / sizeof(ar2[0]);
-    if (n1 == n2)
-        printf("Median is %d", getMedian(ar1, ar2, n1));
-    else
-        printf("Doesn't work for arrays of unequal size");
-    getchar();
-    return 0;
 }
---
+++ PRACTICE/dsa_450/Array/36mediandiffSize.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+double calMedian(int *a, int *b, int n, int m)
+{
+    if (n > m)
+        calMedian(b, a, m, n);
+
+    int start = 0, end = n;
+    int realmed = (m + n + 1) / 2;
+
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+        int leftsize = mid;
+        int rightsize = realmed - mid;
+
+        int leftA = (leftsize > 0) ? a[leftsize - 1] : INT_MIN;
+        int leftB = (rightsize > 0) ? b[rightsize - 1] : INT_MIN;
+
+        int rightA = (leftsize > 0) ? a[leftsize] : INT_MAX;
+        int rightB = (rightsize > 0) ? b[rightsize] : INT_MAX;
+
+        // check if condition matches
+        if (leftA <= rightB and leftB <= rightA)
+        {
+            if ((m + n) % 2 == 0)
+                return (max(leftA, leftB) + min(rightA, rightB)) / 2;
+            return max(leftA, leftB);
+        }
+        else if (leftA > rightB)
+            end = mid - 1;
+        else
+            start = mid + 1;
+    }
+    return 0.0;
+}
+
+int main()
+{
+    int n, m;
+    cout << "Enter array-1 size : ";
+    cin >> n;
+    cout << "Enter array-2 size  : ";
+    cin >> m;
+    cout << "Enter array-1 elements : ";
+    int a[n], b[m];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    cout << "Enter array-2 Elements : ";
+
+    for (int j = 0; j < m; j++)
+        cin >> b[j];
+
+    cout << "Median of array : " << calMedian(a, b, n, m) << endl;
+}
