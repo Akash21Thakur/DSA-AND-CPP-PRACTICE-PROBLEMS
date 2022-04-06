1716caf62e7f1f900f667874a42439b4ade2c781
Akash21Thakur <akashthakur562002@gmail.com>
2022-04-06 20:02:15 +0530

    06-04-2022

---
+++ PRACTICE/dsa_450/Search & Sort/11findTripletSumLessThanK.cpp
@@ -0,0 +1,67 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+	{
+	//method-1 ...naive 3 loop solution
+	// current one ---> n^2
+	public:
+	long long countTriplets(long long arr[], int n, long long sum)
+	{
+	   // i<j<k is just to make confusion...it does not affect the final answer
+	    sort(arr,arr+n);
+	    long long int ans=0;
+	    for(int i=0;i<n-2;i++)
+	    {
+	        int j=i+1,k=n-1;
+	        while(j<k)
+	        {
+	            if(arr[i]+arr[j]+arr[k]>=sum)
+	               k--;
+	               else
+	               {
+	                   ans+=k-j;
+	                   j++;
+	               }
+	        }
+	    }
+	    return ans;
+	}
+		 
+
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+  
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+    	long long sum;
+        cin>>n>>sum;
+        long long arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+	    
+
+       
+
+        Solution ob;
+        cout << ob.countTriplets(arr, n, sum) ;
+	   
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
