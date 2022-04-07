2231e43ef493974d54aa304d176e51cbe98b3f1f
Akash21Thakur <akashthakur562002@gmail.com>
2022-04-07 21:09:14 +0530

    07-04-2022

--- PRACTICE/dsa_450/Search & Sort/0findFirstAndLast.cpp
+++ PRACTICE/dsa_450/Search & Sort/0findFirstAndLast.cpp
@@ -1,43 +1,43 @@
 // { Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
-vector<int> find(int arr[], int n , int x )
+// } Driver Code Ends
+vector<int> find(int arr[], int n, int x)
 {
-    int first=-1,last=-1;
-    int low=0,high=n-1;
-    //finding first
-    while(low<=high)
+    int first = -1, last = -1;
+    int low = 0, high = n - 1;
+    // finding  first
+    while (low <= high)
     {
-        int mid=(low+high)/2;
-        if(arr[mid]>=x)
+        int mid = (low + high) / 2;
+        if (arr[mid] >= x)
         {
-            if(arr[mid]==x)   first=mid;
-            high=mid-1;  //move to left
+            if (arr[mid] == x)
+                first = mid;
+            high = mid - 1; // move to left
         }
         else
-        low=mid+1;
+            low = mid + 1;
     }
-    
-    low=0,high=n-1;
-    
-    //finding last
-    while(low<=high)
+
+    low = 0, high = n - 1;
+
+    // finding last
+    while (low <= high)
     {
-        int mid=(low+high)/2;
-        if(arr[mid]<=x)
+        int mid = (low + high) / 2;
+        if (arr[mid] <= x)
         {
-            if(arr[mid]==x)  last=mid; //move towards right
-            low=mid+1;
+            if (arr[mid] == x)
+                last = mid; // move towards right
+            low = mid + 1;
         }
         else
-         high=mid-1;
+            high = mid - 1;
     }
-    
-    
-    
-    return {first,last};
+
+    return {first, last};
 }
 
 // { Driver Code Starts.
@@ -45,20 +45,19 @@ vector<int> find(int arr[], int n , int x )
 int main()
 {
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
-        int n,x;
-        cin>>n>>x;
-        int arr[n],i;
-        for(i=0;i<n;i++)
-        cin>>arr[i];
+        int n, x;
+        cin >> n >> x;
+        int arr[n], i;
+        for (i = 0; i < n; i++)
+            cin >> arr[i];
         vector<int> ans;
-        ans=find(arr,n,x);
-        cout<<ans[0]<<" "<<ans[1]<<endl;
+        ans = find(arr, n, x);
+        cout << ans[0] << " " << ans[1] << endl;
     }
     return 0;
 }
 
-
-  // } Driver Code Ends
+// } Driver Code Ends
