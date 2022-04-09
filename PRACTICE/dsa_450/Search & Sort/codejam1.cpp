795b4e333df9ec5970ba66796cce81eb644acaae
Akash21Thakur <akashthakur562002@gmail.com>
2022-04-09 22:53:23 +0530

    09-04-2022

---
+++ PRACTICE/dsa_450/Search & Sort/codejam1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int j = 1; j <= t; j++)
+    {
+        cout << "Case #" << j << ":\n";
+        string s;
+        cin >> s;
+        string ans;
+        int flag = 1;
+
+        int n = s.length();
+        int i = 0;
+        for (; i < n - 1; i++)
+        {
+            if (s[i] < s[i + 1])
+            {
+                ans = ans + s[i] + s[i];
+                flag = 0;
+                break;
+            }
+            else if (s[i] == s[i + 1])
+            {
+                ans = ans + s[i] + s[i];
+            }
+            else
+            {
+                ans += s[i];
+            }
+        }
+        if (flag)
+            cout << s << endl;
+        else
+        {
+            if (i == n - 1)
+                cout << (s + s[i]) << endl;
+            else
+            {
+                for (++i; i < n; i++)
+                    ans += s[i];
+                cout << ans << endl;
+            }
+        }
+    }
+}
